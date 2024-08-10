c1a5816708bcbbfe35e574cea71ead8263a67245
JoakimJohnsson <joakim.johnsson@su.se>
2024-08-10 09:34:41 +0200

    initial commit

---
+++ src/main/java/tttai/se/terrific_themed_tinder_ai_backend/TerrificThemedTinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package tttai.se.terrific_themed_tinder_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrificThemedTinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrificThemedTinderAiBackendApplication.class, args);
+	}
+
+}
